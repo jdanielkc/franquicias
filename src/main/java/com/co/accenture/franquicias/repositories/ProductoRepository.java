b95c19d7acb1cfd43f27ac04eaddfd5cd394833c
Jose Daniel Garcia Arias <jd.garciaa1@uniandes.edu.co>
2024-11-29 02:14:50 -0500

    Se agregan comentarios java doc de los repositorios

--- src/main/java/com/co/accenture/franquicias/repositories/ProductoRepository.java
+++ src/main/java/com/co/accenture/franquicias/repositories/ProductoRepository.java
@@ -1,11 +1,21 @@
 package com.co.accenture.franquicias.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.co.accenture.franquicias.entities.Producto;
+import com.co.accenture.franquicias.entities.Sucursal;
 
+/**
+ * Repositorio del producto
+ * 
+ * @version 1.0
+ * @autor Daniel 
+ * @since 2024/11/28
+ */
 @Repository
 public interface ProductoRepository extends JpaRepository<Producto, Integer> {
-
+    Optional<Producto> findTopBySucursalOrderByStockDesc(Sucursal sucursal);
 }
