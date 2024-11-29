decd4679bf889899bf766501d8b813d38e8a7de0
Jose Daniel Garcia Arias <jd.garciaa1@uniandes.edu.co>
2024-11-28 23:14:24 -0500

    Se crea endpoint de creacion de sucursal

--- src/main/java/com/co/accenture/franquicias/controllers/FranquiciaController.java
+++ src/main/java/com/co/accenture/franquicias/controllers/FranquiciaController.java
@@ -8,16 +8,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.co.accenture.franquicias.exceptions.FranquiciaServiceException;
 import com.co.accenture.franquicias.models.request.NuevaFranquiciaRequest;
+import com.co.accenture.franquicias.models.request.NuevaSucursalRequest;
 import com.co.accenture.franquicias.models.response.NuevaFranquiciaResponse;
+import com.co.accenture.franquicias.models.response.NuevaSucursalResponse;
 import com.co.accenture.franquicias.services.IFranquiciaService;
 
 import lombok.AllArgsConstructor;
 
 /**
- * Controlador de la franquicia
+ * Controlador de la API
  * 
- * @version 1.0 - 2024/11/28
+ * @version 1.0
  * @autor Daniel Garcia
+ * @since 2024/11/28
  */
 
 @RestController
@@ -36,9 +39,13 @@ public class FranquiciaController {
      * @throws FranquiciaServiceException
      */
     @PostMapping("/nueva-franquicia")
-    public ResponseEntity<NuevaFranquiciaResponse> addFranquicia(@RequestBody NuevaFranquiciaRequest request)
-            throws FranquiciaServiceException {
+    public ResponseEntity<NuevaFranquiciaResponse> addFranquicia(@RequestBody NuevaFranquiciaRequest request) {
         return franquiciaService.addFranquicia(request);
+    }
+
+    @PostMapping("path")
+    public ResponseEntity<NuevaSucursalResponse> addSucursal(@RequestBody NuevaSucursalRequest request) {
+        return franquiciaService.addSucursal(request);
     }
 
 }
